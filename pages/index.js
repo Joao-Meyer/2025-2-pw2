643d5eecf82aad1caf1b2467203b593e0fe7bcce
joao-meyer <joaoparizotimeyer@gmail.com>
2025-10-06 01:51:54 -0300

    criando listagem de filmes - teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+import { filmes } from '../data/filmes';
+
 function Home () {
-    return <h1>Teste</h1>
+    console.log(filmes);
+
+    const primeiroFilme = filmes[0];
+
+    return (
+        <>
+            <h1>Filmes</h1>
+
+            <ul>
+                <li>
+                    <p>Título: {primeiroFilme.titulo}</p>
+                    <p>Ano: {primeiroFilme.ano}</p>
+                    <p>Gênero: {primeiroFilme.genero}</p>
+                    <p>Nota: {primeiroFilme.nota}</p>
+
+                    <img src={primeiroFilme.poster}/>
+                </li>
+            </ul>
+        </>
+    );
 }
 
 export default Home;
