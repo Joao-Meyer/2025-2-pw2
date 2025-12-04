d67218de05ed3bc59e05dca66c3394304a835ed7
joao h p meyer <56160441+Joao-Meyer@users.noreply.github.com>
2025-12-04 20:20:56 -0300

    recebe os dados da resposta da requisição

--- pages/index.js
+++ pages/index.js
@@ -14,13 +14,13 @@ function Home () {
     const urlVamosChamar = 
     `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=pt-BR&page=1`;
 
-    let resposta = null;
+    let resposta = "batata";
 
     fetch(urlVamosChamar).then(res => {
         resposta = res;
+        
+        console.log(res.json())
     });
-
-    console.log(resposta)
 
     return (
         <>
